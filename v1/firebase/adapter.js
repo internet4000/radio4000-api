6dfc0869c83f62a006b36f5030817ba8a0b8a60e
Oskar Rough <oskar@rough.dk>
2017-02-28 21:21:37 +0100

    Lint using xo

--- v1/firebase/adapter.js
+++ v1/firebase/adapter.js
@@ -1,10 +1,11 @@
+/* eslint camelcase: "off"  */
 var firebase = require('firebase');
 var {serializeChannel, serializeTrack, serializeImage} = require('./serializer.js');
 
 var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -13,7 +14,7 @@ function apiGet(endpoint) {
 	return (
 		firebase.database().ref(endpoint)
 			.once('value')
-	)
+	);
 }
 
 function apiQuery(endpoint, prop, query) {
@@ -21,85 +22,87 @@ function apiQuery(endpoint, prop, query) {
 		firebase.database().ref(endpoint)
 			.orderByChild(prop).equalTo(query)
 			.once('value')
-	)
+	);
 }
 
 function apiGetImage(imageId) {
 	return apiGet(`images/${imageId}`).then(snapshot => {
 		var image = snapshot.val();
 		return serializeImage(image, imageId);
-  })
+	});
 }
 
 function apiGetTrack(trackId) {
 	return apiGet(`tracks/${trackId}`).then(snapshot => {
 		var track = snapshot.val();
 		return serializeTrack(track, trackId);
-  })
+	});
 }
 
 // channel slug or id?
 function apiGetChannel(channelSlug) {
-	return apiQuery('channels','slug', channelSlug).then(snapshot => {
+	return apiQuery('channels', 'slug', channelSlug).then(snapshot => {
 		var val = snapshot.val();
 		var channelId = Object.keys(val)[0];
 		var channel = val[channelId];
 		return serializeChannel(channel, channelId);
-  }).catch(e => {
-		console.log("apigGetChannel error", e);
-  });
+	}).catch(err => {
+		console.log('apigGetChannel error', err);
+	});
 }
 
 function apiGetChannelTracks(channelSlug) {
 	return apiGetChannel(channelSlug).then(channel => {
 		return apiQuery('tracks', 'channel', channel.id).then(snapshot => {
 			var tracks = snapshot.val();
-			var serializedTracks = Object.keys(tracks).map(trackId => serializeTrack(tracks[trackId], trackId));
+			var serializedTracks = Object.keys(tracks)
+				.map(trackId => serializeTrack(tracks[trackId], trackId));
 			return serializedTracks;
 		});
 	});
-};
+}
 
 function apiGetChannels() {
-  return apiGet('channels').then(snapshot => {
+	return apiGet('channels').then(snapshot => {
 		var val = snapshot.val();
-		var channels = Object.keys(val).map(channelId => serializeChannel(val[channelId], channelId));
+		var channels = Object.keys(val)
+			.map(channelId => serializeChannel(val[channelId], channelId));
 		return channels;
-  })
+	});
 }
 
 var FILTERS = {
-  "contains": (channel, val) => channel.indexOf(val) > -1,
-	"icontains": (channel, val) => channel.toLowerCase().indexOf(val.toLowerCase()) > -1,
-  "starts_with": (channel, val) => channel.startsWith(val),
-}
+	contains: (channel, val) => channel.indexOf(val) > -1,
+	icontains: (channel, val) => channel.toLowerCase().indexOf(val.toLowerCase()) > -1,
+	starts_with: (channel, val) => channel.startsWith(val)
+};
 
 function apiGetChannelsFiltered(filters) {
-  var filterFun = function(filter, val) {
-    return function(channel) {
-      var query = filter.split(".");
+	var filterFun = function (filter, val) {
+		return function (channel) {
+			var query = filter.split('.');
 
-      if (query.length == 1) {
-        return channel[filter] == val;
-      }
+			if (query.length === 1) {
+				return channel[filter] === val;
+			}
 
-      if (query.length == 2) {
-        var elem = channel[query[0]];
+			if (query.length === 2) {
+				var elem = channel[query[0]];
 
-        if (!elem) {
-          return false;
-        }
+				if (!elem) {
+					return false;
+				}
 
-        return FILTERS[query[1]](elem, val);
-      }
-      throw Error(`${filter} is not a valid filter.`);
-    }
-  }
-  return apiGetChannels().then(channels =>
-    Object.keys(filters).reduce(
-      (channels, filter) => channels.filter(filterFun(filter, filters[filter])),
-      channels)
-  );
+				return FILTERS[query[1]](elem, val);
+			}
+			throw new Error(`${filter} is not a valid filter.`);
+		};
+	};
+	return apiGetChannels().then(channels =>
+		Object.keys(filters).reduce(
+			(channels, filter) => channels.filter(filterFun(filter, filters[filter])),
+			channels)
+	);
 }
 
 module.exports = {
--- v1/firebase/serializer.js
+++ v1/firebase/serializer.js
@@ -1,12 +1,16 @@
 var {buildCloudinaryUrl} = require('../cloudinary/adapter.js');
 
 function convertHasMany(fromObject) {
-	if (!fromObject) return;
+	if (!fromObject) {
+		return;
+	}
 	return Object.keys(fromObject);
 }
 
-function serializeChannel (channel, channelId) {
-	if (!channel) return;
+function serializeChannel(channel, channelId) {
+	if (!channel) {
+		return;
+	}
 	channel.type = 'channel';
 	channel.id = channelId;
 	channel.tracks = convertHasMany(channel.tracks);
@@ -17,18 +21,26 @@ function serializeChannel (channel, channelId) {
 	return channel;
 }
 
-function serializeTrack (track ,trackId) {
-	if (!track) return;
+function serializeTrack(track, trackId) {
+	if (!track) {
+		return;
+	}
 	track.id = trackId;
 	track.type = 'track';
 	return track;
 }
 
-function serializeImage (image, imageId) {
-	if (!image) return;
+function serializeImage(image, imageId) {
+	if (!image) {
+		return;
+	}
 	image.src = buildCloudinaryUrl(image.src);
 	image.id = imageId;
 	return image;
 }
 
-module.exports = {serializeChannel, serializeTrack, serializeImage};
+module.exports = {
+	serializeChannel,
+	serializeTrack,
+	serializeImage
+};
